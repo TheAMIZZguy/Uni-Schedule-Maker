2d20a8d22707a2a3ac100a34c43ae9f036818201
TheAMIZZguy <amizz3000@gmail.com>
2021-03-16 00:49:00 -0600

    (P3, T3, 5) Like 10 hours of work and trial and error, code has less functionality than before somehow and I am more confused.

--- src/main/ui/CourseAdder.java
+++ src/main/ui/CourseAdder.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
+import static java.lang.Integer.max;
+
 public class CourseAdder extends JPanel implements ActionListener, FocusListener {
 
     JTextField nameField;
@@ -40,20 +42,20 @@ public class CourseAdder extends JPanel implements ActionListener, FocusListener
     public CourseAdder() {
         setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
 
-        numSubCoursesField.setValue(2);
-        numLabsField.setValue(2); //todo
-        numTutorialsField.setValue(2);
+        numSubCoursesField.setText("01");
+        numLabsField.setValue("00"); //todo
+        numTutorialsField.setValue("00");
 
         changeSizeFields();
 
-        JPanel leftHalf = new JPanel() {
+        JPanel leftHalf = new JPanel(); //{
             //Don't allow us to stretch vertically.
-            public Dimension getMaximumSize() {
-                Dimension pref = getPreferredSize();
-                return new Dimension(Integer.MAX_VALUE,
-                        pref.height);
-            }
-        };
+//            public Dimension getMaximumSize() {
+//                Dimension pref = getPreferredSize();
+//                return new Dimension(Integer.MAX_VALUE,
+//                        pref.height);
+//            }
+//        };
         leftHalf.setLayout(new BoxLayout(leftHalf,
                 BoxLayout.PAGE_AXIS));
         leftHalf.add(createEntryFields());
@@ -64,21 +66,38 @@ public class CourseAdder extends JPanel implements ActionListener, FocusListener
     }
 
     private void changeSizeFields() {
-        subCourseNameFields = new JTextField[(int) numSubCoursesField.getValue()];
-        subCourseStartTimeFields = new JFormattedTextField[(int) numSubCoursesField.getValue()];
-        subCourseEndTimeFields = new JFormattedTextField[(int) numSubCoursesField.getValue()];
-        subCourseDayFields = new JFormattedTextField[(int) numSubCoursesField.getValue()]; //todo
+        subCourseNameFields = new JTextField[cleanTxt(numSubCoursesField.getText())];
+        subCourseStartTimeFields = new JFormattedTextField[cleanTxt(numSubCoursesField.getText())];
+        subCourseEndTimeFields = new JFormattedTextField[cleanTxt(numSubCoursesField.getText())];
+        subCourseDayFields = new JFormattedTextField[cleanTxt(numSubCoursesField.getText())]; //todo
+        for (int i = 0; i < cleanTxt(numSubCoursesField.getText()); i++) {
+            subCourseNameFields[i] = new JFormattedTextField();
+            subCourseStartTimeFields[i] = new JFormattedTextField();
+            subCourseEndTimeFields[i] = new JFormattedTextField();
+            subCourseDayFields[i] = new JFormattedTextField();
+        }
 
-        labNameFields = new JTextField[(int) numLabsField.getValue()];
-        labStartTimeFields = new JFormattedTextField[(int) numLabsField.getValue()];
-        labEndTimeFields = new JFormattedTextField[(int) numLabsField.getValue()];
-        labDayFields = new JFormattedTextField[(int) numLabsField.getValue()]; //todo
+        labNameFields = new JTextField[cleanTxt(numLabsField.getText())];
+        labStartTimeFields = new JFormattedTextField[cleanTxt(numLabsField.getText())];
+        labEndTimeFields = new JFormattedTextField[cleanTxt(numLabsField.getText())];
+        labDayFields = new JFormattedTextField[cleanTxt(numLabsField.getText())]; //todo
+        for (int i = 0; i < cleanTxt(numLabsField.getText()); i++) {
+            labNameFields[i] = new JFormattedTextField();
+            labStartTimeFields[i] = new JFormattedTextField();
+            labEndTimeFields[i] = new JFormattedTextField();
+            labDayFields[i] = new JFormattedTextField();
+        }
 
-        tutorialNameFields = new JTextField[(int) numTutorialsField.getValue()];
-        tutorialStartTimeFields = new JFormattedTextField[(int) numTutorialsField.getValue()];
-        tutorialEndTimeFields = new JFormattedTextField[(int) numTutorialsField.getValue()];
-        tutorialDayFields = new JFormattedTextField[(int) numTutorialsField.getValue()]; //todo
-
+        tutorialNameFields = new JTextField[cleanTxt(numTutorialsField.getText())];
+        tutorialStartTimeFields = new JFormattedTextField[cleanTxt(numTutorialsField.getText())];
+        tutorialEndTimeFields = new JFormattedTextField[cleanTxt(numTutorialsField.getText())];
+        tutorialDayFields = new JFormattedTextField[cleanTxt(numTutorialsField.getText())]; //todo
+        for (int i = 0; i < cleanTxt(numTutorialsField.getText()); i++) {
+            tutorialNameFields[i] = new JFormattedTextField();
+            tutorialStartTimeFields[i] = new JFormattedTextField();
+            tutorialEndTimeFields[i] = new JFormattedTextField();
+            tutorialDayFields[i] = new JFormattedTextField();
+        }
     }
 
     protected JComponent createButtons() {
@@ -234,38 +253,111 @@ public class CourseAdder extends JPanel implements ActionListener, FocusListener
         }
     }
 
-    //Needed for FocusListener interface.
-    public void focusLost(FocusEvent e) { } //ignore
-
     protected JComponent createEntryFields() {
         JPanel panel = new JPanel(new SpringLayout());
 
         String[] labelStrings = {
                 "Name: ",
-                "Sub Course Name: ",
-                "Start Time: ",
+                "Number of Sub Courses: ",
+                "Number of Labs: ",
+                "Number of Tutorials: "
         };
 
         JLabel[] labels = new JLabel[labelStrings.length];
         JComponent[] fields = new JComponent[labelStrings.length];
-        int fieldNum = 0;
 
         //todo
         //Create the text field and set it up.
-        nameField = new JTextField();
-        nameField.setColumns(20);
-        fields[fieldNum++] = nameField;
-
-        subCourseNameFields[0] = new JTextField();
-        subCourseNameFields[0].setColumns(20);
-        fields[fieldNum++] = subCourseNameFields[0];
-
-        subCourseStartTimeFields[0] = new JFormattedTextField(
-                createFormatter("#####"));
-        fields[fieldNum++] = subCourseStartTimeFields[0];
+        setTextFields(fields, 0);
 
         //Associate label/field pairs, add everything,
         //and lay it out.
+        associateLabels(panel, labelStrings, labels, fields);
+
+        SpringUtilities.makeCompactGrid(panel,
+                labelStrings.length, 2,
+                GAP, GAP, //init x,y
+                GAP, GAP / 2);//xpad, ypad
+        return panel;
+    }
+
+    protected JComponent createRefreshedFields() {
+        JPanel panel = new JPanel(new SpringLayout());
+        int numSub = 1;
+        int numLab = 0;
+        int numTut = 0;
+
+        if (cleanTxt(numSubCoursesField.getText()) != 0) {
+            numSub = max(cleanTxt(numSubCoursesField.getText()), 1);
+        }
+
+        if (cleanTxt(numLabsField.getText()) != 0) {
+            numLab = cleanTxt(numLabsField.getText());
+        }
+
+        if (cleanTxt(numTutorialsField.getText()) != 0) {
+            numTut = cleanTxt(numTutorialsField.getText());
+        }
+
+        String[] labelStrings = new String[4 + numSub * 4 + numLab * 4 + numTut * 4];
+
+        labelStrings[0] = "Name: ";
+        labelStrings[1] = "Number of Sub Courses: ";
+        addVariableFields(numSub * 4, labelStrings, 2, "Sub Course");
+        labelStrings[2 + numSub * 4] = "Number of Labs: ";
+        addVariableFields(numLab * 4, labelStrings, 3 + numSub * 4, "Lab");
+        labelStrings[3 + ((numLab > 0) ? 1 : 0) + numSub * 4 + numLab * 4] = "Number of Tutorials: ";
+        addVariableFields(numTut * 4, labelStrings, 4 + ((numLab > 0) ? 1 : 0)
+                + numSub * 4 + numLab * 4, "Tutorial");
+
+        JLabel[] labels = new JLabel[labelStrings.length];
+        JComponent[] fields = new JComponent[labelStrings.length];
+
+        changeSizeFields();
+        //todo
+        //Create the text field and set it up.
+        setTextFieldsRefresh(fields, 0);
+
+        //Associate label/field pairs, add everything,
+        //and lay it out.
+        associateLabels(panel, labelStrings, labels, fields);
+
+        SpringUtilities.makeCompactGrid(panel,
+                labelStrings.length, 2,
+                GAP, GAP, //init x,y
+                GAP, GAP / 2);//xpad, ypad
+
+        //revalidate();
+        return panel;
+    }
+
+    private int cleanTxt(String text) {
+        if (text.equals("  ") || text.equals("") || text.equals(" ")) {
+            return 0;
+        } else if (text.charAt(0) == ' ') {
+            return (int) text.charAt(1);
+        } else if (text.charAt(1) == ' ') {
+            return (int) text.charAt(0);
+        } else {
+            return Integer.parseInt(text);
+        }
+    }
+
+    private void addVariableFields(int num, String[] labelStrings, int startIndex, String type) {
+        for (int i = startIndex; i < num + startIndex; i++) {
+            if (i - startIndex % 4 == 0) {
+                labelStrings[i] = type + " Name: ";
+            } else if (i - startIndex % 4 == 1) {
+                labelStrings[i] = "Start Time: ";
+            } else if (i - startIndex % 4 == 2) {
+                labelStrings[i] = "End Time: ";
+            } else {
+                labelStrings[i] = "Days: ";
+            }
+        }
+    }
+
+    private void associateLabels(JPanel panel, String[] labelStrings, JLabel[] labels, JComponent[] fields) {
         for (int i = 0; i < labelStrings.length; i++) {
             labels[i] = new JLabel(labelStrings[i],
                     JLabel.TRAILING);
@@ -276,18 +368,118 @@ public class CourseAdder extends JPanel implements ActionListener, FocusListener
             //Add listeners to each field.
             JTextField tf = null;
             if (fields[i] instanceof JSpinner) {
-                tf = getTextField((JSpinner)fields[i]);
+                tf = getTextField((JSpinner) fields[i]);
             } else {
-                tf = (JTextField)fields[i];
+                tf = (JTextField) fields[i];
             }
             tf.addActionListener(this);
             tf.addFocusListener(this);
         }
-        SpringUtilities.makeCompactGrid(panel,
-                labelStrings.length, 2,
-                GAP, GAP, //init x,y
-                GAP, GAP / 2);//xpad, ypad
-        return panel;
+    }
+
+    private void setTextFields(JComponent[] fields, int fieldNum) {
+        nameField = new JTextField();
+        nameField.setColumns(20);
+        fields[fieldNum++] = nameField;
+
+        //This can be easily extracted
+        numSubCoursesField = new JFormattedTextField(createFormatter("##"));
+        numSubCoursesField.setColumns(20);
+        numSubCoursesField.setName("subCourses");
+        fields[fieldNum++] = numSubCoursesField;
+        for (int i = 0; i < cleanTxt(numSubCoursesField.getText()); i++) {
+            fields[fieldNum++] = subCourseNameFields[i];
+            fields[fieldNum++] = subCourseStartTimeFields[i];
+            fields[fieldNum++] = subCourseEndTimeFields[i];
+            fields[fieldNum++] = subCourseDayFields[i];
+        }
+
+        numLabsField = new JFormattedTextField(createFormatter("##"));
+        numLabsField.setName("labs");
+        fields[fieldNum++] = numLabsField;
+        for (int i = 0; i < cleanTxt(numLabsField.getText()); i++) {
+            fields[fieldNum++] = labNameFields[i];
+            fields[fieldNum++] = labStartTimeFields[i];
+            fields[fieldNum++] = labEndTimeFields[i];
+            fields[fieldNum++] = labDayFields[i];
+        }
+
+        numTutorialsField = new JFormattedTextField(createFormatter("##"));
+        numTutorialsField.setName("tutorials");
+        fields[fieldNum++] = numTutorialsField;
+        for (int i = 0; i < cleanTxt(numTutorialsField.getText()); i++) {
+            fields[fieldNum++] = tutorialNameFields[i];
+            fields[fieldNum++] = tutorialStartTimeFields[i];
+            fields[fieldNum++] = tutorialEndTimeFields[i];
+            fields[fieldNum++] = tutorialDayFields[i];
+        }
+    }
+
+    private void setTextFieldsRefresh(JComponent[] fields, int fieldNum) {
+        //nameField = new JTextField();
+        //nameField.setColumns(20);
+        fields[fieldNum++] = nameField;
+
+        //This can be easily extracted
+        //numSubCoursesField = new JFormattedTextField(createFormatter("##"));
+        //numSubCoursesField.setColumns(20);
+        //numSubCoursesField.setName("subCourses");
+        fields[fieldNum++] = numSubCoursesField;
+        for (int i = 0; i < cleanTxt(numSubCoursesField.getText()); i++) {
+            fields[fieldNum++] = subCourseNameFields[i];
+            fields[fieldNum++] = subCourseStartTimeFields[i];
+            fields[fieldNum++] = subCourseEndTimeFields[i];
+            fields[fieldNum++] = subCourseDayFields[i];
+        }
+
+        //numLabsField = new JFormattedTextField(createFormatter("##"));
+        //numLabsField.setName("labs");
+        fields[fieldNum++] = numLabsField;
+        for (int i = 0; i < cleanTxt(numLabsField.getText()); i++) {
+            fields[fieldNum++] = labNameFields[i];
+            fields[fieldNum++] = labStartTimeFields[i];
+            fields[fieldNum++] = labEndTimeFields[i];
+            fields[fieldNum++] = labDayFields[i];
+        }
+
+        //numTutorialsField = new JFormattedTextField(createFormatter("##"));
+        //numTutorialsField.setName("tutorials");
+        fields[fieldNum++] = numTutorialsField;
+        for (int i = 0; i < cleanTxt(numTutorialsField.getText()); i++) {
+            fields[fieldNum++] = tutorialNameFields[i];
+            fields[fieldNum++] = tutorialStartTimeFields[i];
+            fields[fieldNum++] = tutorialEndTimeFields[i];
+            fields[fieldNum++] = tutorialDayFields[i];
+        }
+    }
+
+    public void focusLost(FocusEvent e) {
+        //System.out.println(e.getSource().getClass());
+        if (e.getSource().getClass().equals(JFormattedTextField.class)) {
+            refreshNumFields((JFormattedTextField) e.getSource());
+            JPanel leftHalf = new JPanel();
+            leftHalf.setLayout(new BoxLayout(leftHalf,
+                    BoxLayout.PAGE_AXIS));
+            leftHalf.add(createEntryFields());
+            leftHalf.add(createButtons());
+
+            add(leftHalf);
+            add(createAddressDisplay());
+        }
+    }
+
+    void refreshNumFields(JFormattedTextField jte) {
+        //System.out.println(jte.getText());
+        System.out.println(jte.getName());
+        //int fields = Integer.parseInt(jte.getText());
+        createRefreshedFields();
+//        if (jte.getName().equals("subCourses")) {
+//            //reset subcourses fields
+//        } else if (jte.getName().equals("labs")) {
+//            //reset the lab fields
+//        } else {
+//            //reset the tutorial fields
+//        }
     }
 
     public JFormattedTextField getTextField(JSpinner spinner) {
