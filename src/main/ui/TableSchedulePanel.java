882aa1e0bfc577324433652f8589844e773952be
Andres <andreszp@students.cs.ubc.ca>
2021-03-22 20:27:02 -0400

    (P3, T3, 20) Button, MISSING USER STORIES SAVE/LOAD and DELETE/FILTER SCHEDS

--- src/main/ui/TableSchedulePanel.java
+++ src/main/ui/TableSchedulePanel.java
@@ -3,22 +3,47 @@ package ui;
 import model.Scheduler;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 
-public class TableSchedulePanel extends JPanel {
+public class TableSchedulePanel extends JPanel implements ActionListener {
 
     ArrayList<Scheduler> schedules;
+
+    private JButton deleteSpecified;
+    JComboBox numSchedBox;
 
     public TableSchedulePanel(ArrayList<Scheduler> schedules) {
         super(new GridLayout(0,1));
 
         this.schedules = schedules;
 
-//        if (schedules.size() == 0) {
-//            schedules.add(new Scheduler(0));
-//        }
+        deleteSpecified = new JButton("Delete Schedule At Index");
+        deleteSpecified.setActionCommand("Delete");
+        deleteSpecified.addActionListener(this);
+
+        int size = schedules.size();
+        String[] numSchedsArr = new String[size];
+        for (int i = 0; i < size; i++) {
+            numSchedsArr[i] = String.valueOf(i);
+        }
+
+        numSchedBox = new JComboBox(numSchedsArr);
+
+        if (schedules.size() == 0) {
+            addScheduleTable(new Scheduler(0), "0");
+        }
+
+        JPanel listButtonPanel = new JPanel();
+        listButtonPanel.setLayout(new BoxLayout(listButtonPanel, BoxLayout.X_AXIS));
+        listButtonPanel.setPreferredSize(new Dimension(200,20));
+        listButtonPanel.add(numSchedBox);
+        listButtonPanel.add(deleteSpecified);
+
+        add(listButtonPanel);
 
         for (int i = 0; i < schedules.size(); i++) {
             addScheduleTable(schedules.get(i), Integer.toString(i));
@@ -46,5 +71,10 @@ public class TableSchedulePanel extends JPanel {
         JFrame frame = new JFrame("SimpleTableDemo");
         this.setOpaque(true); //content panes must be opaque
         frame.setContentPane(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(numSchedBox.getSelectedIndex());
     }
 }
